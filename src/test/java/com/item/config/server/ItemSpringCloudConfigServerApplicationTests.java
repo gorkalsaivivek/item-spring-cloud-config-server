dbf12548d91334c54eae20ae5bf6c73b77ebbc08
Vivek <saivivekgorkal@gmail.com>
2023-06-01 23:00:56 -0400

    first commit

---
+++ src/test/java/com/item/config/server/ItemSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.item.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemSpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
